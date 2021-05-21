4b2dbaff332f5cf7a75b1c04a14bb25a496f3fd9
caiobrreis <cbrr@cesar.school>
2021-05-21 20:16:37 -0300

    Refactored code

--- Assets/Scripts/ColoredWords.cs
+++ Assets/Scripts/ColoredWords.cs
@@ -1,18 +1,20 @@
+using System;
 using System.Diagnostics;
 using UnityEngine;
 using TMPro;
+using Random = UnityEngine.Random;
 
-public class MathOperations : MonoBehaviour
+public class ColoredWords : MonoBehaviour
 {
     [SerializeField] private Transform gameCanvas;
     [SerializeField] private TextMeshProUGUI roundCountField;
-    [SerializeField] private TextMeshProUGUI fullOperationField;
+    [SerializeField] private TextMeshProUGUI generatedWordField;
     [SerializeField] private TMP_InputField inputField;
-
-    private int symbolIdx;
-    private int n1;
-    private int n2;
-    private int result;
+    
+    private int generatedWordIdx;
+    private int generatedColorIdx;
+    private int prevGeneratedWordIdx = -1;
+    private int prevGeneratedColorIdx = -1;
 
     private byte round = 1;
     private byte totalRounds = 5;
@@ -20,7 +22,16 @@ public class MathOperations : MonoBehaviour
     private int correctGuesses = 0;
     private float worstSpeedPerRound = 20000f;
 
-    private static string[] symbols = new string[] { "+", "-", "*" };
+    public static string[] words = new string[] {
+        "Vermelho", "Laranja", "Amarelo", "Verde",
+        "Azul", "Rosa", "Roxo", "Marrom"
+    };
+
+    public static Color32[] colors = new Color32[] {
+        new Color32(255,0,0,255), new Color32(255,136,0,255), new Color32(255,216,0,255),
+        new Color32(0,255,33,255), new Color32(0,107,255,255), new Color32(255,0,220,255),
+        new Color32(87,0,127,255), new Color32(100,70,0,255)
+    };
 
     Stopwatch stopwatch = new Stopwatch();
 
@@ -37,7 +48,7 @@ public class MathOperations : MonoBehaviour
         Common.common.KeepInputOpen(inputField);
 
         if (Input.GetKeyDown(KeyCode.Return) && inputField.text != "") {
-            if (inputField.text.Equals(result.ToString())) {
+            if (inputField.text.Equals(words[generatedColorIdx], StringComparison.CurrentCultureIgnoreCase)) {
                 Common.common.ShowOutline(true, gameCanvas);
                 correctGuesses++;
             } else {
@@ -61,26 +72,16 @@ public class MathOperations : MonoBehaviour
         roundCountField.text = $"round {round.ToString()}/{totalRounds.ToString()}";
         inputField.text = "";
 
-        symbolIdx = Random.Range(0, symbols.Length);
-        n1 = Random.Range(1, 99);
-        n2 = symbolIdx == 1 ? Random.Range(1, n1) : Random.Range(1, 99);
+        do { generatedWordIdx = Random.Range(0, words.Length); }
+        while (generatedWordIdx == prevGeneratedWordIdx);
 
-        fullOperationField.text = $"{n1.ToString()} {symbols[symbolIdx]} {n2.ToString()} =";
+        do { generatedColorIdx = Random.Range(0, colors.Length); } 
+        while (generatedColorIdx == generatedWordIdx || generatedColorIdx == prevGeneratedColorIdx);
 
-        result = GetResult(symbolIdx);
-    }
+        generatedWordField.text = words[generatedWordIdx];
+        generatedWordField.color = colors[generatedColorIdx];
 
-    int GetResult(int symbolIdx)
-    {
-        switch (symbolIdx) {
-            case 0:
-                return n1 + n2;
-            case 1:
-                return n1 - n2;
-            case 2:
-                return n1 * n2;
-            default:
-                return 0;
-        }
+        prevGeneratedWordIdx = generatedWordIdx;
+        prevGeneratedColorIdx = generatedColorIdx;
     }
 }
---
+++ Assets/Scripts/Common.cs
@@ -0,0 +1,67 @@
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Common : MonoBehaviour
+{
+    public static Common common;
+    public int prevScene;
+
+    public GameObject redOutline;
+    public GameObject greenOutline;
+    public float outlineDuration = 0.5f;
+
+    [HideInInspector] public int speedScore;
+    [HideInInspector] public int correctGuessesScore;
+
+    void Awake()
+    {
+        if (common == null) {
+            DontDestroyOnLoad(gameObject);
+            common = this;
+        } else if (common != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void KeepInputOpen(TMP_InputField inputField)
+    {
+        inputField.ActivateInputField();
+        inputField.Select();
+    }
+
+    public void ShowOutline(bool correct, Transform parent)
+    {
+        if (correct) {
+            GameObject obj = Instantiate(greenOutline, parent);
+            Destroy(obj, outlineDuration);
+        } else {
+            GameObject obj = Instantiate(redOutline, parent);
+            Destroy(obj, outlineDuration);
+        }
+    }
+
+    public void CalculateSpeed(long time, float worstSpeed)
+    {
+        float t = Mathf.Clamp01(time / worstSpeed);
+        speedScore = (int)Mathf.Lerp(500f, 0f, t);
+    }
+
+    public void WriteToTextFile(long time)
+    {
+        string path = "Assets/Data/Score.txt";
+
+        StreamWriter writer = new StreamWriter(path); // (path, true) to append
+        // falta nome
+        writer.WriteLine($"{DateTime.Now}, {correctGuessesScore}, {speedScore}, {correctGuessesScore + speedScore}, {time}");
+        writer.Close();
+    }
+
+    public void ResultScene()
+    {
+        prevScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene("Result");
+    }
+}
